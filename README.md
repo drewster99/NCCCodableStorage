f6e2923347e7ede716d4260aabc5a130513e336d
Andrew Benson <drewbenson@netjack.com>
2020-05-20 22:06:35 -0500

    Minor nothings

--- README.md
+++ README.md
@@ -1,3 +1,51 @@
 # NCCCodableStorage
 
-A description of this package.
+**Automatic Codable storage to/from JSON**
+Twitter *@DrewsterBenson*
+
+Whenever a defaultValue is given, **NCCCodableStorage** will attempt to load the initial value from the existing JSON at initialization time.  If a saved value cannot be loaded or decoded, the defaultValue is used as the initial value.
+
+As above, any changes are immediately and synchronously written to storage.
+
+```swift
+    struct MyData: Codable {
+        var name: String = "Mister Default"
+        var age: Double = 29
+    }
+    
+    @NCCCodableStorage(defaultValue: MyData(), filename: "myData.json") var myData: MyData
+```
+
+At initialization, it will attempt to read an array of struct **MyData** from **myDataArray.json**.  If it cannot be read or decoded, the *defaultValue*, in this case an empty array, will initially be used.
+
+Using **updateMode: .afterIdle(5.0)** will write updates back to the **myDataArray.json** storage only after the *wrappedValue* has not changed in at least 5.0 seconds.
+
+```swift
+    @NCCCodableStorage(defaultValue: [],
+                       filename: "myDataArray.json",
+                       updateMode: .afterIdle(5.0)) var myDataArray: [MyData]
+```
+
+Initializing using **initialValue: ...** is the same as including it after the declaration.
+
+```swift
+    // This:
+    @NCCCodableStorage(initialValue: 8.675309, url: someURL) var jenny: Double
+    
+    // is equivalent to this:
+    @NCCCodableStorage(url: someURL) var jenny: Double = 8.675309
+```
+
+We can initialize with manual value updates instead of automatic also.  Data can then be manually loaded with:  **_someData.load()** or manually saved with: ** _someData.save()**.
+
+```swift
+    @NCCCodableStorage(filename: "someData.json", updateMode: .manual) var someData: [MyData] = []
+```
+
+Creates the full path for the file based onthe given directoryType.
+
+```swift
+    @NCCCodableStorage(filename: "someFileName.json", directoryType: .desktopDirectory)
+    private var fooooo: [String] = ["hello", "there"]
+```
+
--- Tests/NCCCodableStorageTests/NCCCodableStorageTests.swift
+++ Tests/NCCCodableStorageTests/NCCCodableStorageTests.swift
@@ -3,6 +3,46 @@ import Foundation
 
 @testable import NCCCodableStorage
 
+class Foo {
+    // someString will have initial value of "Hello!" and all changes will be synchronously
+    // written to "someString.json" immediately every time someString is updated.
+    //
+    // The file will be placed in the documents directory in the userDomainMask
+    @NCCCodableStorage(filename: "someString.json") var someString: String = "Hello!"
+
+    // Whenever a defaultValue is given, NCCCodableStorage will attempt to load the initial value
+    // from the existing JSON at initialization time.  If a saved value cannot be loaded or decoded,
+    // the defaultValue is used as the initial value.
+    //
+    // As above, any changes are immediately and synchronously written to storage.
+    struct MyData: Codable {
+        var name: String = "Mister Default"
+        var age: Double = 29
+    }
+    @NCCCodableStorage(defaultValue: MyData(), filename: "myData.json") var myData: MyData
+
+    // At initialization, it will attempt to read an array of struct MyData from "myDataArray.json".
+    // If it cannot be read or decoded, the defaultValue, in this case an empty array, will initially
+    // be used.
+    //
+    // Using "updateMode: .afterIdle(5.0)" will write updates back to the myDataArray.json storage only
+    // after the wrappedValue has not changed in at least 5.0 seconds.
+    @NCCCodableStorage(defaultValue: [],
+                       filename: "myDataArray.json",
+                       updateMode: .afterIdle(seconds: 5.0)) var myDataArray: [MyData]
+    
+    // We can initialize with manual value updates instead of automatic also.
+    //
+    // Data can then be manually loaded with:  _someData.load()
+    // or manually saved with:  _someData.save()
+    @NCCCodableStorage(filename: "someData.json", updateMode: .manual) var someData: [MyData] = []
+
+    // Creates the full path for the file based onthe given directoryType.
+    @NCCCodableStorage(filename: "someFileName.json", directoryType: .desktopDirectory)
+    private var fooooo: [String] = ["hello", "there"]
+}
+
+
 final class NCCCodableStorageTests: XCTestCase {
     // Can't wait until we can use propertywrappers inside of functions etc..
     @NCCCodableStorage(initialValue: "hello",
@@ -10,8 +50,11 @@ final class NCCCodableStorageTests: XCTestCase {
                        directoryType: .documentDirectory,
                        updateMode: .manual) var string: String
 
+    @NCCCodableStorage(defaultValue: 10.0, filename: "someDouble.json") var someDouble: Double
+
     // Super weak test -- don't hate me.  or do.  I dont' care.
     func simpleCodableStorageTest() {
+        XCTFail("Screwed")
         try? FileManager.default.removeItem(at: _string.url)
 
         string = "craziness"
@@ -29,7 +72,9 @@ final class NCCCodableStorageTests: XCTestCase {
             return
         }
         XCTAssertEqual(string, "craziness", "String loaded '\(string)' != string saved previously 'craziness'")
-        
+
+        print("All done.  string at \(_string.url.absoluteString)")
+        print("All done.  someDouble at \(_someDouble.url.absoluteString)")
     }
 
     static var allTests = [
